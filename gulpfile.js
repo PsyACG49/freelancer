e28881fbee8d9bb2ba272928ce2bb357fe64b268
AcgCoder <testo_acg@hotmail.com>
2022-01-14 18:59:39 -0600

    agregando sass

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const { src, dest, watch} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
+
+
+function css( done ) {
+    console.log('Compilado SASS...');
+    src('src/scss/**/*.scss') // Identificar el archivo .scss a compilar
+      .pipe( plumber() ) //No detiene la ejecucion si hay algun error
+      .pipe( sass() ) // Compilarlo
+      .pipe( dest('build/css') ) // Almacenamiento en el disco duro
+    done();
+}
+
+function dev ( done ) {
+    watch('src/scss/**/*.scss', css); // Que la ejecucion no se pare y ste mirando cambios
+
+    done();
+}
+
+exports.css = css;
+exports.dev = dev;
